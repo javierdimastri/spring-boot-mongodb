f195a8ecc6ddbe9916264239fe50e39dad14163a
hilmantrihatmojo <mohammed.hilmantrihatmojo@btpn.com>
2022-12-28 19:56:53 +0700

    [hilman] add test for update in abc service and repository

--- src/test/java/com/javierdimastri/service/AbcServiceImplTest.java
+++ src/test/java/com/javierdimastri/service/AbcServiceImplTest.java
@@ -2,6 +2,7 @@ package com.javierdimastri.service;
 
 import com.javierdimastri.model.Abc;
 import com.javierdimastri.repository.AbcRepository;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -44,5 +45,22 @@ public class AbcServiceImplTest {
         abcService.saveAbc(description, name);
 
         verify(abcRepository, times(1)).save(firstCreatedAbc);
+    }
+
+    @Test
+    public void changeAbcBy_shouldCallUpdateAbcByFromRepository_whenInvoked() {
+        String abcId = "5cc5e9914184de8673d7e1d1";
+        final ObjectId ABC_ID = new ObjectId(abcId);
+        Abc abcPayload = new Abc("test name", "test description");
+        Abc updatedAbc = Abc.builder()
+                .id(ABC_ID).name(abcPayload.getName()).description(abcPayload.getDescription())
+                .build();
+        when(abcRepository.updateAbcBy(ABC_ID, abcPayload)).thenReturn(updatedAbc);
+
+        Abc actualResult = abcService.changeAbcBy(abcId, abcPayload);
+
+        verify(abcRepository, times(1)).updateAbcBy(ABC_ID, abcPayload);
+        assertThat(actualResult).isEqualTo(updatedAbc);
+
     }
 }
